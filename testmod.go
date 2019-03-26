aa6aa6172cb7f7d0c103ed30ac80baec79d11ddf
Josh Sandlin <joshua.sandlin@gmail.com>
2019-03-26 15:35:01 -0400

    updated to be multi language

--- testmod.go
+++ testmod.go
@@ -1,7 +1,21 @@
 package testmod
 
-import "fmt"
+import (
+  "errors"
+  "fmt"
+)
 
-func Hi(name string) string {
-  return fmt.Sprintf("Hi, %s!", name)
+func Hi(name, lang string) (string, error) {
+  switch lang {
+  case "en":
+    return fmt.Sprintf("Hi, %s!", name)
+  case "pt":
+    return fmt.Sprintf("Oi, %s!", name), nil
+  case "es":
+    return fmt.Sprintf("¡Hola, %s!", name), nil
+  case "fr":
+    return fmt.Sprintf("Bonjour, %s!", name), nil
+  default:
+    return "", errors.New("Unknown language")
+  }
 }
