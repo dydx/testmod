6b66a29fea37f36599025961ca12584c8b14a140
Josh Sandlin <joshua.sandlin@gmail.com>
2019-03-26 15:37:51 -0400

    corrected for a missing return value

--- testmod.go
+++ testmod.go
@@ -8,7 +8,7 @@ import (
 func Hi(name, lang string) (string, error) {
   switch lang {
   case "en":
-    return fmt.Sprintf("Hi, %s!", name)
+    return fmt.Sprintf("Hi, %s!", name), nil
   case "pt":
     return fmt.Sprintf("Oi, %s!", name), nil
   case "es":
