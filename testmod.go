5e26f8148b4c9664f98337a2e7a98470bcd37f6b
Josh Sandlin <joshua.sandlin@gmail.com>
2019-03-26 16:41:50 -0400

    Added unit tests

--- testmod.go
+++ testmod.go
@@ -1,21 +1,27 @@
 package testmod
 
 import (
-  "errors"
-  "fmt"
+	"errors"
+	"fmt"
 )
 
+// Hi accepts a String for `name` and a String for `lang`
+// Accepted values of `lang` are limited to:
+// * `en`
+// * `pt`
+// * `es`
+// * `fr`
 func Hi(name, lang string) (string, error) {
-  switch lang {
-  case "en":
-    return fmt.Sprintf("Hi, %s!", name), nil
-  case "pt":
-    return fmt.Sprintf("Oi, %s!", name), nil
-  case "es":
-    return fmt.Sprintf("¡Hola, %s!", name), nil
-  case "fr":
-    return fmt.Sprintf("Bonjour, %s!", name), nil
-  default:
-    return "", errors.New("Unknown language")
-  }
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "pt":
+		return fmt.Sprintf("Oi, %s!", name), nil
+	case "es":
+		return fmt.Sprintf("¡Hola, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", errors.New("Unknown language")
+	}
 }
